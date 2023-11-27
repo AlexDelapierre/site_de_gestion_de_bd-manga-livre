d8277b9c49355f12887c4be61e94fc2db906d2d6
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-11-27 20:26:03 +0100

    Add bootstrap cards

--- index.php
+++ index.php
@@ -56,6 +56,47 @@
           </div>
         </div>
       </div>
+
+      <div class="row text-center">
+
+        <div class="col-md-4 my-2 d-flex">
+          <div class="card">
+            <img src="uploads/books/1-1984.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 my-2 d-flex">
+          <div class="card">
+            <img src="uploads/books/2-histoires-courtes.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 my-2 d-flex">
+          <div class="card">
+            <img src="uploads/books/3-zai-zai-zai-zai.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </main>
 
     <footer class="py-3 my-4">
