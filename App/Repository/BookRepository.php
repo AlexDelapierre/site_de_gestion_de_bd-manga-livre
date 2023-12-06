3551fab0f011c58ff5b481eba75e6d84b3854f62
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-12-06 20:38:20 +0100

    Add Mysql.php and config.php

--- App/Repository/BookRepository.php
+++ App/Repository/BookRepository.php
@@ -3,12 +3,15 @@
 namespace App\Repository;
 
 use App\Entity\Book;
+use App\Db\Mysql;
 
 class BookRepository
 {
   public function findOneById(int $id)
   {
     // Appel bdd
+    $mysql = Mysql::getInstance();
+    var_dump($mysql);
     $book = ['id' => 1, 'title' => 'titre test', 'description' => 'description test'];
 
     $bookEntity = new Book();
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  return [
+    'db_name' => 'bd/manga/livre',
+    'db_user' => 'root',
+    'db_password' => '',
+    'db_port' => '3306',
+    'db_host' => 'localhost',
+  ];
