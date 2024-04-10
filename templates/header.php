8b6a63b92b8e040b3640ab84be23cbdfe4a323c6
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2024-04-10 19:54:01 +0200

    Add logo in Bootstrap navbar

--- templates/header.php
+++ templates/header.php
@@ -19,7 +19,7 @@
       class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <div class="col-md-3 mb-2 mb-md-0">
         <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-
+          <img width="90" src="assets/images/logo-bookeo.jpg" alt="Logo Bookeo">
         </a>
       </div>
 
