5907a4efc6ea33c8e8a1b08ab189ff92e338e1dd
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2024-08-20 22:06:10 +0200

    update carousel.js

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -7,29 +7,74 @@ const carouselArray = document.getElementsByClassName('carousel-container');
   const next = element.querySelector(".next");
   const track = element.querySelector(".track");
   const cards = element.getElementsByClassName('card-container');
-  let width = carousel.offsetWidth;
-  let index = 0;
 
-  window.addEventListener("resize", function () {
-    width = carousel.offsetWidth;
+  // Initialise une variable pour stocker la largeur d'une card
+  let cardWidth;
+
+  // Initialise une variable pour stocker la somme des largeurs
+  let totalWidth = 0;
+
+  // Parcourt chaque élément et ajoute sa largeur à la somme totale
+  [...cards].forEach(card => {
+    cardWidth = card.offsetWidth;
+    totalWidth += card.offsetWidth;
   });
+
+  // Initialise une variable pour stocker la largeur de la pise du carousel
+  let trackWidth = track.offsetWidth;
+
+  // Initialise une variable pour stocker l'index de l'avancée du carousel'
+  let index = 0;
   
+  // console.log(cardWidth);
+  // console.log(trackWidth);
+  // console.log(totalWidth);
+
+  // // Stocker la largeur actuelle de la fenêtre
+  // let windowWidth = window.innerWidth;
+
+  // // Surveiller les redimensionnements pour mettre à jour la variable windowWidth
+  // window.addEventListener('resize', function() {
+  //   windowWidth = window.innerWidth;
+  // });
+
+  // // Vérifier si la fenêtre a été redimensionnée
+  // if (window.innerWidth !== windowWidth) {
+  //   index = 0;  // Réinitialiser l'index à 0
+  //   track.style.transform = "translateX(0px)";  // Revenir à la première carte
+  //   windowWidth = window.innerWidth;  // Mettre à jour la largeur de la fenêtre
+  // }
+
+  window.addEventListener('resize', function() {
+    track.style.transform = "translateX(0px)";  // Revenir à la première carte
+    index = 0;  // Réinitialiser l'index à 0
+    trackWidth = track.offsetWidth; // Réassigne la valeur de la largeur de la track
+    prev.classList.remove("show");
+    prev.classList.add("hide");
+    next.classList.remove("hide");
+    next.classList.add("show");
+    console.log(index);
+  });
+
   if (cards.length <= 4) {
     next.classList.add("display");
   }
 
   if (cards.length > 4) {
-    // next.classList.add("hide");
-
     next.addEventListener("click", function (e) {
       e.preventDefault();
       index = index + 1;
       prev.classList.add("show");
-      track.style.transform = "translateX(" + index * -width + "px)";
-      if (track.offsetWidth - index * width < index * width) {
+      track.style.transform = "translateX(" + index * -cardWidth + "px)";
+  
+      console.log(index);
+
+      // Cacher le bouton 'next' si on atteint la fin
+      if (trackWidth + index * cardWidth >= totalWidth) {
         next.classList.add("hide");
+        }
       }
-    });
+    );
   };
 
   prev.addEventListener("click", function () {
@@ -38,7 +83,7 @@ const carouselArray = document.getElementsByClassName('carousel-container');
     if (index === 0) {
       prev.classList.remove("show");
     }
-    track.style.transform = "translateX(" + index * -width + "px)";
+    track.style.transform = "translateX(" + index * -trackWidth + "px)";
   });
   
 });
